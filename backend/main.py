10689d317519d4084d1d27f4b921e3ee0839bacb
Wahid Sholihul Fikri <wahidsf1905@gmail.com>
2025-04-18 19:54:37 +0700

    Fix CORS origin

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["http://localhost:3000"]
+origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
